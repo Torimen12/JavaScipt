c25785452cea829b1a2f32abbd657d4e95d912f6
TriMen <tranminhtri1998@gmail.com>
2021-01-16 18:21:59 +0700

    second commit

--- Form_Validation/js.js
+++ Form_Validation/js.js
@@ -14,9 +14,14 @@ function Run(){
         onInput(formControl[i]);
     }
     form.onsubmit = function(e){
+        var result=true;
         e.preventDefault();
         for (var i = 0; i < formControl.length; i++) {
             checkInputs(formControl[i]);
+            result= checkInputs(formControl[i])&&result;
+        }
+        if(result){
+            console.log(getInfo(formControl[0],formControl[1], formControl[2]));
         }
     }
 }
@@ -30,35 +35,45 @@ function checkOnblur(option) {
 
 //Kiểm tra input
 function checkInputs(option) {
+    var result = false ;
     if (isBlank(option)) {
         setErrorFor(option, "This field can not be blank!");
+        result = false ;
     }
     else if (option === email) {
         if (isEmail(option)) {
             setSuccessFor(option);
+            result = true ;
         } else {
             setErrorFor(option, "Couldn't find your email!");
+            result= false ;
         }
     }
     else if (option === password) {
         if (!setMinLength(option, 8)) {
             setErrorFor(option, "Use 8 characters or more for your password!");
+            result= false ;
         }
         else {
             setSuccessFor(option);
+            result = true ;
         }
     }
     else if (option === passwordConfirm) {
         if (!checkPasswordConfirm(option, password)) {
             setErrorFor(option, "Those passwords didn't match!");
+            result= false ;
         }
         else {
             setSuccessFor(option);
+            result = true ;
         }
     }
     else {
         setSuccessFor(option);
+        result = true ;
     }
+    return result;
 }
 
 //Kiểm tra trường có để trống 
@@ -104,4 +119,13 @@ function setSuccessFor(input) {
     var formMessage = formGroup.querySelector(".form-message");
     formMessage.innerText = '';
     formGroup.className = 'form-group success';
+}
+
+function getInfo(name, email, password){
+    var info = {
+        name : name.value,
+        email : email.value,
+        password : password.value
+    };
+    return info;
 }
